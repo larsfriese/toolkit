c9c6fe100ac4b906bec436f5684ffbd18053cdb9
lars.friese@onlinehome.de <lars.friese@onlinehome.de>
2019-12-29 23:40:47 +0100

    Small code fix.

--- scripts/network_scanner.py
+++ scripts/network_scanner.py
@@ -12,14 +12,19 @@ def iwconfig():
 
     out = returned_output.decode('utf-8')
     splitted = out.split()
-
-    for i in splitted:
-        if '192.168' in i:
-            index = splitted.index(i)
-            if 'inet' in splitted[index-1]:
-                ip_split = i.split('.')
-                subnet = ip_split[2]
-                return str(subnet)
+    
+    try:
+        for i in splitted:
+            if '192.168' in i:
+                index = splitted.index(i)
+                if 'inet' in splitted[index-1]:
+                    ip_split = i.split('.')
+                    subnet = ip_split[2]
+                    return str(subnet)
+    except:
+        print('Subnet cant be figured out. Type in subnet manually (192.168.x.0-255):\n')
+        subnet_input = raw_input()
+        return str(subnet_input)
 
 def main():
     nm = nmap.PortScanner()
